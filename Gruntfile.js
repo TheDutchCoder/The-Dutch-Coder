7afd307216a8d12ae4dd8d6f466f77aa8f7f6a66
TheDutchCoder <rp.kaper@gmail.com>
2014-12-07 20:10:37 -0500

    Added SASS and Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,30 @@ module.exports = function(grunt) {
 
         pkg: grunt.file.readJSON('package.json'),
 
+        // Sass.
+        sass: {
+            dev: {
+                files: {
+                    'css/style.css' : 'sass/style.scss'
+                }
+            },
+            prod: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'css/style.css' : 'sass/style.scss'
+                }
+            }
+        },
+
+        // Autoprefix CSS.
+        autoprefixer: {
+            no_dest: {
+                src: 'css/style.css'
+            }
+        },
+
         // Concatenate JS files.
         concat: {
             dist: {
@@ -22,15 +46,30 @@ module.exports = function(grunt) {
                 src: 'js/scripts.js',
                 dest: 'dist/js/scripts.min.js'
             }
+        },
+
+        // Watch files.
+        watch: {
+            css: {
+                files: '**/*.scss',
+                tasks: ['sass']
+            }
         }
 
     });
 
     // Load tasks.
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-autoprefixer');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Define task shortcuts.
-    grunt.registerTask('default', ['concat', 'uglify']);
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('js', ['concat', 'uglify']);
+
+    grunt.registerTask('dev', ['sass:dev', 'autoprefixer', 'concat']);
+    grunt.registerTask('prod', ['sass:prod', 'autoprefixer', 'concat', 'uglify']);
 
 };
